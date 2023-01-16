a265fa8fd7fb5470f2bed4accd5c34dd572e0caa
kristijan-kresic-hvar <kristijan.kresic.dev@gmail.com>
2023-01-16 01:14:37 +0100

    Added real data from api to devCard, bug fixes, ui fixes

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,19 +1,51 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./Search.scss";
 
-function Search({ onSearch }) {
+function Search({ onSearch, noResults, onChange, loading }) {
   const inputRef = useRef(null);
+
+  const [lastSearch, setLastSearch] = useState("");
+
+  const handleKeyDown = (e) => {
+    const currentSearch = inputRef.current.value;
+    if (lastSearch === currentSearch) return;
+
+    if (e.key === "Enter") {
+      onSearch(currentSearch);
+      setLastSearch(currentSearch);
+    }
+  };
+
+  const handleClick = () => {
+    const currentSearch = inputRef.current.value;
+    if (lastSearch === currentSearch) return;
+
+    onSearch(currentSearch);
+    setLastSearch(currentSearch);
+  };
+
+  if (noResults) inputRef.current.value = "";
+
+  const disabledStyle = loading ? { pointerEvents: "none", opacity: 0.5 } : {};
+
   return (
     <div className="search">
       <input
         ref={inputRef}
+        onChange={onChange}
+        onKeyDown={handleKeyDown}
         type="text"
         name="q"
-        placeholder="Search GitHub username…"
+        placeholder={noResults ? "" : "Search GitHub username…"}
       />
       <div className="search__right">
-        <p className="search__right__error">No results</p>
-        <button onClick={() => onSearch(inputRef.current.value)} type="button">
+        {noResults && <p className="search__right__error">No results</p>}
+        <button
+          style={disabledStyle}
+          disabled={loading}
+          onClick={handleClick}
+          type="button"
+        >
           Search
         </button>
       </div>
